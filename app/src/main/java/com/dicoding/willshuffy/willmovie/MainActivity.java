f1ce8162df005e0fc6aa4a440b581e0979c75dcb
willshuffy <saputrawilly26@gmail.com>
2018-10-05 16:31:11 +0700

    setup recyclerview adapter and load a few dummy data

--- app/src/main/java/com/dicoding/willshuffy/willmovie/MainActivity.java
+++ app/src/main/java/com/dicoding/willshuffy/willmovie/MainActivity.java
@@ -2,17 +2,69 @@ package com.dicoding.willshuffy.willmovie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.dicoding.willshuffy.willmovie.adapter.SearchAdapter;
 import com.dicoding.willshuffy.willmovie.mvp.MainPresenter;
 import com.dicoding.willshuffy.willmovie.mvp.MainView;
+import com.dicoding.willshuffy.willmovie.mvp.model.upcoming.ResultsItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.support.v7.widget.RecyclerView.VERTICAL;
 
 public class MainActivity extends AppCompatActivity implements MainView {
+
+    @BindView(R.id.et_search)
+    EditText et_search;
+
+    @BindView(R.id.btn_search)
+    Button btn_search;
+
+    @BindView(R.id.rv_movielist)
+    RecyclerView rv_movielist;
+
+    private SearchAdapter adapter;
+    private List<ResultsItem> list=new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
         MainPresenter presenter=new MainPresenter(this);
+
+        setupList();
+        loadDummyData();
+    }
+
+    private void setupList(){
+        adapter=new SearchAdapter();
+        rv_movielist.addItemDecoration(new DividerItemDecoration(this, VERTICAL));
+        rv_movielist.setLayoutManager(new LinearLayoutManager(this));
+        rv_movielist.setAdapter(adapter);
+    }
+
+    private void loadDummyData(){
+        list.clear();
+        for (int i = 0; i<= 10; i++){
+            ResultsItem item=new ResultsItem();
+            item.setTitle("Title"+i);
+            item.setOverview("Overview"+i);
+            item.setReleaseDate("2018-03-19");
+            list.add(item);
+        }
+        adapter.replaceAll(list);
     }
 }
---
+++ app/src/main/java/com/dicoding/willshuffy/willmovie/adapter/SearchViewHolder.java
@@ -0,0 +1,40 @@
+package com.dicoding.willshuffy.willmovie.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dicoding.willshuffy.willmovie.R;
+import com.dicoding.willshuffy.willmovie.mvp.model.upcoming.ResultsItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SearchViewHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.img_poster)
+    ImageView img_poster;
+
+    @BindView(R.id.tv_title)
+    TextView tv_title;
+
+    @BindView(R.id.tv_overview)
+    TextView tv_overview;
+
+    @BindView(R.id.tv_release_date)
+    TextView tv_release_date;
+
+    public SearchViewHolder(View itemView){
+        super(itemView);
+
+        ButterKnife.bind(this, itemView);
+    }
+
+    public void bind(ResultsItem item){
+        tv_title.setText(item.getTitle());
+        tv_overview.setText(item.getOverview());
+        tv_release_date.setText(item.getReleaseDate());
+    }
+
+}
